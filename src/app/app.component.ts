c3757117d542924c0f0fee3884dc8415dbad4bb3
Chandan <chandan.k.samanta@hotmail.co.in>
2024-04-29 19:07:11 +0530

    Revert "New UI Added"
    This reverts commit 4b75f68536e7197042ab7ddfb44c08d66d5809f3.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,4 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'bits-devops-assignment';
-  ngAfterContentInit() {
-    this.loadScript('assets/script.js');
-    this.loadScript('assets/s3-service.js');
-  }
-  public loadScript(url:string) {
-    let body = <HTMLDivElement>document.body;
-    let script = document.createElement('script');
-    script.innerHTML = '';
-    script.src = url;
-    script.async = true;
-    script.defer = true;
-    body.appendChild(script);
-  }
 }
